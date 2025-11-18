ca30a96378a8d3c9bbd6d49fb2b3c6928a9474fa
Vishal Gawhade <gavhadevishal5@gmail.com>
2025-11-18 21:26:32 +0530

    change-color

--- js/script.js
+++ js/script.js
@@ -1,33 +1,14 @@
-// this are the pillars of JS DOM
-// selectionnn 
-// // var h1  = document.querySelector('h1')
-// console.log(h1);
+var btn = document.querySelector('button')
+var box = document.querySelector('#box')
 
-//changing HTMl
-// var h1  = document.querySelector('h1')
-// h1.innerHTML = 'I am batmen'
 
-//changing CSS
-// var h1  = document.querySelector('h1')
-// h1.style.color = 'red'
-
-//Event listners
-// var h1  = document.querySelector('h1')
-// h1.addEventListener('dblclick' ,function(){
-//     console.log('Hellooooo!!!');
-// })
-
-var inc = document.querySelector('#inc')
-var dec = document.querySelector('#dec')
-var h2 = document.querySelector('h2')
-var a = 0;
-inc.addEventListener('click', function(){
-    a++
-    h2.innerHTML = a
+btn.addEventListener('click', function(){
+    var c1  = Math.floor(Math.random()*256)
+    var c2  = Math.floor(Math.random()*256)
+    var c3  = Math.floor(Math.random()*256)
     
-})
-dec.addEventListener('click', function(){
-    a--
-    h2.innerHTML = a
+    box.style.backgroundColor  = `rgb(${c1} , ${c2}, ${c3})`
+    
+    
     
 })
