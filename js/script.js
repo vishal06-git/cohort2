91cc2b16891e1a669c09a7459c564d896c2a3861
Vishal Gawhade <gavhadevishal5@gmail.com>
2025-11-22 01:40:26 +0530

    creating an element

--- js/script.js
+++ js/script.js
@@ -1,14 +1,35 @@
 var btn = document.querySelector('button')
-var box = document.querySelector('#box')
+var main = document.querySelector('main')
 
 
 btn.addEventListener('click', function(){
-    var c1  = Math.floor(Math.random()*256)
-    var c2  = Math.floor(Math.random()*256)
-    var c3  = Math.floor(Math.random()*256)
+    var div = document.createElement('div')
+
+    var x = Math.random()*100
+    var y = Math.random()*100
+    var r = Math.random()*360
+    var scl = Math.random()*2
     
-    box.style.backgroundColor  = `rgb(${c1} , ${c2}, ${c3})`
+
+    var c1 = Math.floor(Math.random()*226)
+    var c2 = Math.floor(Math.random()*226)
+    var c3 = Math.floor(Math.random()*226)
+
     
+
+    div.style.height = '50px'
+    div.style.width = '50px'
+    div.style.position = 'absolute'
+    div.style.background = `rgb(${c1},${c2},${c3})`
+
+
+    div.style.left = x + '%'
+    div.style.top = y + '%'
+    div.style.rotate = r + 'deg'
+    div.style.scale = scl 
+
     
+
+    main.appendChild(div)
     
 })
